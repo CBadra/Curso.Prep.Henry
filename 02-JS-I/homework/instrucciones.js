982aaa8966a633d94501189ef71a8984724ab3fb
camila <camim2089@gmail.com>
2022-04-13 14:56:23 -0300

    Homework js I con comprension de conceptos

---
+++ 02-JS-I/homework/instrucciones.js
@@ -0,0 +1,9 @@
+variables: Es un espacio , como una especie de caja donde guardo cosas que despues quiero utilizar.;
+String: son palabras que aparecen en un idioma que habla la computadora, y para que ella entienda lo que hablamos las personas se colocan con "":
+Funciones: Son cosas que le indicamos a la computadora, para que cuando nosotros depues le recordemos el nombre con el que le explicamos la funcion,
+ esta haga lo que le pedimos por ejemplo si le pedimos que reste. Le decimos acordate que si te digo resta , tu a un numero "x" le vas a restar otro.;
+ Declaraciones: Es cuando le pedimos a la computadora que nos ayude con la logica a ver si algo pasa o no pasa, por ejemplo, preguntarle si el perro es blanco
+ que me avise y si no es blanco tambien que me avise, pero con otra oracion para poder distinguirlo;
+ Valores Booleanos: Le hablamos de nuestro a nuestra pc para que eliga entre dos opciones, por ejemplo que me diga si yo puedo volver por mi misma
+ y ela me va a responder si es que realmente puedo o no.
+ 
