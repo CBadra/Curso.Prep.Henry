2a903900c808fba3a580b53656ef20d8bb41881c
camila <camim2089@gmail.com>
2022-04-27 18:01:03 -0300

    Homework js IV

---
+++ 05-JS-IV/homework/conceptos.js
@@ -0,0 +1,16 @@
+* Objetos: son todas aquellas cosas que js tiene dentro como si fuera una casa, el objeto seria la taza solo que no sepueden tocar
+
+* Propiedades: son como los adjetvos del objeto, es decir tengo una tasa y dentro de las cosas que tiene la taza, es blanca
+La propiedad debe ser un identificador válido de JavaScript, una secuencia alfanumérica de caracteres,
+incluyendo también el guión bajo ("_") y el signo dolar ("$"), que no puede comenzar por un número.
+Por ejemplo, objeto.$1 es válido, mientras que objeto.1 no lo es EN LA NOTACION POR PUNTOS. En la notacion por corchetes puede
+tener cualquier valor.
+
+
+* Métodos: son las funciones que tengo dentro del objeto, algo que le puedo decir que quiero que haga, en el ejemplo de la taza la funcion podria ser tomar cafe
+
+* Bucle `for…in`: es un bucle que va recorriendo dentro de los objetos en un lugar que le indiquemos, itera sobre todas las propiedades enumerables de un
+objeto que está codificado por cadenas (ignorando los codificados por Símbolo) y se escribe for (const property in object)
+
+* Notación de puntos vs notación de corchetes: ambos dan acceso a las propiedades y métodos de un objeto, y se lo llamada asi:
+objeto.propiedad  o  objeto[nombre_propiedad]
